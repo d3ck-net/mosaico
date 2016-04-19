13a91a6a92a3762765992b5da813cd26b17e0afb
Jan Manuel Schlieper <manuel.schlieper@beyondrm.com>
2016-04-19 15:23:28 +0200

    added simple beyond template

--- server-config.js
+++ server-config.js
@@ -1,9 +1,9 @@
 // Put here your email sending configurations!
 module.exports = {
 	emailTransport: {
-		// host: 'yoursmtpserver',
-		// port: 25,
-		// auth: { user: '####', pass: '####' }
+		host: 'mail.beyondrm.com',
+		port: 25,
+		auth: { user: 'manuel.schlieper@beyondrm.com', pass: 'hut30spiegel9' }
 	},
 	emailOptions: {
 		from: 'Mosaico by VOXmail <test@mosaico.io>', // sender address
